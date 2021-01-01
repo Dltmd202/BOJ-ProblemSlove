acafc851a62dbb0107b8002b1b1aeed8d22f3a64
Dltmd202 <dltmd202@gmail.com>
2021-01-01 20:10:41 +0900

    Permutations with replacement with BackTracking

--- 15651/main.py
+++ 15651/main.py
@@ -11,3 +11,22 @@ for p in product((range(1,n+1)),repeat=m):
         print(i ,end=' ')
     print()
 
+
+data = range(1,n+1)
+visit = [False]*(n+1)
+
+def solution(select : list , m):
+    if m == 0:
+        for i in select:
+            print(i,end=' ' )
+        print()
+        return
+
+    for i in range(1,n+1):
+        visit[i] = True
+        select.append(i)
+        solution(select,m-1)
+        visit[i] = False
+        select.pop()
+
+solution([],m)
