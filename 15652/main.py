902fad443a34e198391f1a671da74f666d3eb4b4
Dltmd202 <dltmd202@gmail.com>
2021-01-01 20:21:38 +0900

    Combinations with replacement with BackTracking

--- 15652/main.py
+++ 15652/main.py
@@ -11,3 +11,24 @@ for i in combination:
     for j in i:
         print(j,end=' ')
     print()
+
+
+data = range(1,n+1)
+visit = [False]*(n+1)
+
+def solution(select : list , m , idx):
+    if m == 0 :
+        for i in select:
+            print(i ,end=' ')
+        print()
+        return
+
+    for i in range(idx,n+1):
+        visit[i] = True
+        select.append(i)
+        solution(select,m-1,i)
+        visit[i] =False
+        select.pop()
+
+
+solution([],m,1)
