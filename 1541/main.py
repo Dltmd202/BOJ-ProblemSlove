0881ad1bb973839019a56aebf2c06dedb37955ff
Dltmd202 <dltmd202@gmail.com>
2021-01-18 06:38:01 +0900

    Greedy

---
+++ 1541/main.py
@@ -0,0 +1,14 @@
+#1541
+d = input().split('-')
+num = []
+
+for i in d:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
