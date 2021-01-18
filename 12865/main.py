1a149e7a4ad60f8df73ef71673d7f788a7d5db40
Dltmd202 <dltmd202@gmail.com>
2021-01-19 08:42:58 +0900

    Knapsack

---
+++ 12865/main.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n, w = map(int, input().split())
+bag = [tuple(map(int,input().split())) for _ in range(n)]
+
+
+knap = [0 for _ in range(w+1)]
+
+for i in range(n):
+    for j in range(w, 1, -1):
+        if bag[i][0] <= j:
+            knap[j] = max(knap[j], knap[j-bag[i][0]] + bag[i][1])
+    print(knap)
+print(knap[-1])
