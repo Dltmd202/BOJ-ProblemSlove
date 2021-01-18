ecd17841652a8304d69c4b2d998f7dc53ba9a089
Dltmd202 <dltmd202@gmail.com>
2021-01-19 07:18:14 +0900

    Hash with Integer

--- 10816/main.py
+++ 10816/main.py
@@ -3,15 +3,24 @@
 
 MAX =int(1e7)
 
+def index(x):
+    if x >= 0:
+        return x+MAX
+    else:
+        return MAX+x
+
+
+
 n = int(input())
 first = list(map(int,input().split()))
 m = int(input())
 second = list(map(int,input().split()))
 
-hash = [0]*(MAX+1)
+hash = [0]*(MAX*2+1)
+
 
 for i in range(n):
-    hash[first[i]] += 1
+    hash[index(first[i])] += 1
 
 for i in range(m):
-    print(hash[second[i]],end=' ')
+    print(hash[index(second[i])],end=' ')
