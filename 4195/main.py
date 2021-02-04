3e7a8c1fb5e0833d252056b7ba845615ec0be6dc
Dltmd202 <dltmd202@gmail.com>
2021-02-05 08:58:12 +0900

    Disjoint Set

---
+++ 4195/main.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b, cnt):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a != b:
+        parent[b] = a
+        cnt[a] += cnt[b]
+
+
+
+
+for tb in range(int(input())):
+    f = int(input())
+    parents = {}
+    cnt = {}
+    for _ in range(f):
+        friend1, friend2 = input().strip().split()
+
+        if friend1 not in parents:
+            parents[friend1] = friend1
+            cnt[friend1] = 1
+
+        if friend2 not in parents:
+            parents[friend2] = friend2
+            cnt[friend2] = 1
+
+        union_parent(parents, friend1, friend2, cnt)
+        print(cnt[find_parent(parents, friend1)])
