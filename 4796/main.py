6e6b7c961cb70cf368d345e6393ceb88fa23b7d0
Dltmd202 <dltmd202@gmail.com>
2021-02-06 07:31:21 +0900

    Greedy

--- 4796/main.py
+++ 4796/main.py
@@ -4,5 +4,5 @@ while True:
     l, p, v = map(int,input().split())
     if l == 0 and p == 0 and v == 0:
         break
-    print(f"Case {case}: {(v % p ) + l * (v // p)}")
+    print(f"Case {case}: {min(v % p, l) + l * (v // p)}")
     case += 1
