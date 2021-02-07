cdaa450ec00c4a4938f4ceaf84bca28829324532
Dltmd202 <dltmd202@gmail.com>
2021-02-07 19:41:20 +0900

    Back Tracking

--- 1182/main.py
+++ 1182/main.py
@@ -1,22 +1,19 @@
+import sys
+input = sys.stdin.readline
 
-n, s = map(int,input().split())
-data = list(map(int,input().split()))
-data.sort()
-# print(data)
-second =1
-sum = data[0]
-cnt = 0
-
-for first in range(n-1):
-    while second < n and sum < s :
-        # print(first , second , sum)
-        if data[second] > 0 and data[second] > - data[first] and second > first + 1:
-            break
-        sum += data[second]
-        second += 1
+def find(idx, sum):
+    global cnt
+    if idx >= len(data):
+        return
+    sum += data[idx]
     if sum == s:
         cnt += 1
-    sum -= data[first]
+    find(idx + 1, sum - data[idx])
+    find(idx + 1, sum)
 
-    # print("first++",first +1 , second , sum)
+
+cnt = 0
+n, s = map(int, input().split())
+data = list(map(int, input().split()))
+find(0, 0)
 print(cnt)
