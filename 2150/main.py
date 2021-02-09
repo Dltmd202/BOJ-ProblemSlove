3ae6684b78748dd6b7305f3fe943af2523709c14
Dltmd202 <dltmd202@gmail.com>
2021-02-09 17:58:39 +0900

    SCC

--- 2150/main.py
+++ 2150/main.py
@@ -1,15 +1,12 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(int(1e6))
 MAX = 10001
 
 v, e = map(int, input().split())
 
-graph = [[] for _ in range(v + 1)]
-stack = deque()
-finish = [False] * MAX
+graph = [[] for _ in range(MAX)]
+finished = [False] * MAX
 parent = [0] * MAX
+stack = deque()
 SCC = []
 id = 1
 
@@ -24,32 +21,29 @@ def dfs(start):
     for will in graph[start]:
         if parent[will] == 0:
             result = min(result, dfs(will))
-        elif not finish[will]:
+        elif not finished[will]:
             result = min(result, parent[will])
 
-    if result == parent[start]:
+    if parent[start] == result:
         scc = []
         while True:
             top = stack.pop()
             scc.append(top)
-            finish[top] = True
+            finished[top] = True
             if top == start:
                 break
         SCC.append(sorted(scc))
     return result
 
-for i in range(e):
+
+for _ in range(e):
     a, b = map(int, input().split())
     graph[a].append(b)
-
 
 for i in range(v):
     if parent[i] == 0:
         dfs(i)
-SCC.sort()
 print(len(SCC) - 1)
+for scc in sorted(SCC[1:]):
+    print(' '.join(str(comp) for comp in scc),-1)
 
-for scc in SCC[1:]:
-    for comp in scc:
-        print(comp, end=' ')
-    print(-1)
