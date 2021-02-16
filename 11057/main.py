d801f949d3baad214d79e092f4b8de1373ce9a33
Dltmd202 <dltmd202@gmail.com>
2021-02-16 16:55:11 +0900

    DP

---
+++ 11057/main.py
@@ -0,0 +1,11 @@
+MAX = 10007
+n = int(input())
+
+dp = [[1] * 10 for _ in range(n + 2)]
+
+for i in range(2, n + 1):
+    dp[i][0] = sum(dp[i - 1])
+    for j in range(1, 10):
+        dp[i][j] = (dp[i][j - 1] - dp[i -1][j - 1]) % MAX
+
+print(sum(dp[n]) % MAX)
