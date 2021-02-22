d9509f013d7ca12e91fd64821bc54073b7b3757f
Dltmd202 <dltmd202@gmail.com>
2021-02-23 02:28:36 +0900

    BFS

--- 3055/main.py
+++ 3055/main.py
@@ -1,84 +1,44 @@
-import heapq
 from collections import deque
-import math
 
-n , m = map(int,input().split())
-graph =['.'*(m+1)]
+c, r = map(int, input().split())
+graph = [list(input()) for _ in range(c)]
+visit = [[-1] * r for _ in range(c)]
+dy = [0, -1, 0, 1]
+dx = [1, 0, -1, 0]
 
-dy = [ 0 , -1 , 0 , 1]
-dx = [1 , 0 , -1 , 0]
+q = deque()
+water = []
+s = []
+for i in range(c):
+    for j in range(r):
+        if graph[i][j] == '*':
+            water.append((i, j))
+            visit[i][j] = 0
+        elif graph[i][j] == 'S':
+            s.append((i, j))
+            visit[i][j] = 0
 
-d = deque()
-water = deque()
-s = deque()
-rock = deque()
-
-visit =[[0]*(m+1) for _ in range(n+1)]
-
-wq = []
-
-for i in range(1,n+1):
-    data = '.'+input()
-    for j in range(m+1):
-        if data[j] == 'D':
-            d.append((i,j))
-        elif data[j] == '*':
-            water.append((i,j))
-            visit[i][j] = -1
-            heapq.heappush(wq,(-1,i,j))
-        elif data[j] == 'X':
-            rock.append((i,j))
-        elif data[j] == 'S':
-            s.append((i,j))
-
-    graph.append(data)
+for w in water:
+    q.append(w)
+q.append(*s)
 
 
+def bfs():
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < c and 0 <= nx < r and not graph[ny][nx] == 'X':
+                if graph[ny][nx] == 'D' and graph[y][x] == 'S':
+                    return True, str(visit[y][x] + 1)
+                if graph[ny][nx] == 'D' and graph[y][x] == '*':
+                    continue
+                if graph[ny][nx] == '.' and visit[ny][nx] == -1:
+                    graph[ny][nx] = graph[y][x]
+                    visit[ny][nx] = visit[y][x] + 1
+                    q.append((ny, nx))
+    return False, "KAKTUS"
 
-while wq:
-    cost , y , x = heapq.heappop(wq)
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 1 <= ny <= n and 1 <= nx <= m :
-            if graph[ny][nx] == '.' and visit[ny][nx] == 0:
-                heapq.heappush(wq,(visit[ny][nx],ny,nx))
-                visit[ny][nx] = visit[y][x] -1
 
-
-
-# for i in range(1,n+1):
-#     for j in range(1,m+1):
-#         print(visit[i][j], end=' ')
-#     print()
-
-dq = deque()
-y , x =s.popleft()
-visit[y][x] = 1
-dq.append((y,x))
-answer = 0
-while dq:
-    y ,x = dq.popleft()
-    for i in range(4) :
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 1 <= ny <= n and 1 <= nx <= m :
-            if graph[ny][nx] == '.' and visit[ny][nx] <= 0 and visit[y][x] + 1 < abs(visit[ny][nx]):
-                visit[ny][nx] = (visit[y][x] + 1)
-                dq.append((ny ,nx))
-            elif graph[ny][nx] == 'D' :
-                visit[ny][nx] = visit[y][x] +1
-                dq.append((ny,nx))
-
-# print()
-# for i in range(1,n+1):
-#     for j in range(1,m+1):
-#         print(visit[i][j], end=' ')
-#     print()
-
-y ,x =d.popleft()
-
-if visit[y][x] ==   0:
-    print("KAKTUS")
-else:
-    print(visit[y][x] -1 )
+print(bfs()[1])
