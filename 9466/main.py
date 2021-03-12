e9e45b87f685714cb4a11bf5c0db5ea147160cfa
Dltmd202 <dltmd202@gmail.com>
2021-03-12 22:49:49 +0900

    DFS cycle search

---
+++ 9466/main.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+
+for tc in range(int(input())):
+    n = int(input())
+    data = [0] + list(map(int, input().split()))
+    visit = [False] * (n + 1)
+    result = []
+
+    def dfs(start):
+        visit[start] = True
+        stack.append(start)
+        will = data[start]
+
+        if visit[will]:
+            if will in stack:
+                while True:
+                    top = stack[-1]
+                    stack.pop()
+                    result.append(top)
+                    if will == top:
+                        break
+        else:
+            dfs(will)
+
+    for i in range(1, n + 1):
+        if not visit[i]:
+            stack = []
+            dfs(i)
+    print(n - len(result))
