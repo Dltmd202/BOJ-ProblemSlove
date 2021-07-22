34e707b996f06dac1730b34d48182a605dec87d4
Dltmd202 <dltmd202@gmail.com>
2021-07-22 22:31:00 +0900

    hash

---
+++ 17219/main.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+accounts = dict()
+
+for i in range(n):
+    ids, p = (input().rstrip()).split()
+    accounts[ids] = p
+
+for j in range(m):
+    ids = input().rstrip()
+    print(accounts[ids])
