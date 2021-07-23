5e61da1f2ce013f500fc40efe850187acaecd7cc
Dltmd202 <dltmd202@gmail.com>
2021-07-23 16:13:24 +0900

    tree search

--- 1167/main.py
+++ 1167/main.py
@@ -6,23 +6,30 @@ n = int(input())
 graph = [[] for _ in range(n + 1)]
 ind = [0] * (n + 1)
 visit = [False] * (n + 1)
-for i in range(1, n + 1):
-    data = list(map(int, input().split()))
-    for v in range(1, len(data) - 1, 2):
-        graph[i].append((data[v], data[v + 1]))
-
-q = deque()
-q.append((1, 0))
-visit[1] = True
-answer = 0
+res = 0
+for i in range(n):
+    line = list(map(int, input().split()))
+    for j in range(1, len(line) - 1, 2):
+        graph[line[0]].append([line[j + 1], line[j]])
+        graph[line[j]].append([line[j + 1], line[0]])
 
 
-while q:
-    now, length = q.popleft()
-    answer = max(answer, length)
-    for v, cost in graph[now]:
-        if not visit[v]:
-            visit[v] = True
-            q.append((v, length + cost))
+def search(start):
+    global res
+    visit[start] = True
+    left, right = 0, 0
+    for c, will in graph[start]:
+        depth = 0
+        if not visit[will]:
+            depth = search(will)
+            if depth + c > left:
+                right = left
+                left = depth + c
+            elif depth + c > right:
+                right = depth + c
+    res = max(res, left + right)
+    return max(left, right)
 
-print(answer)
+
+search(1)
+print(res)
