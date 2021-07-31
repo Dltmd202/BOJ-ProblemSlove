b02557bce0ab3b5d5f19100473624dfb2fa84729
Dltmd202 <dltmd202@gmail.com>
2021-08-01 04:58:40 +0900

    divide

---
+++ 5639/main.py
@@ -0,0 +1,31 @@
+
+
+data = []
+
+
+def div(left, right):
+    if left > right:
+        return
+
+    root = data[left]
+    idx = left + 1
+    while idx <= right:
+        if data[idx] > root:
+            break
+        idx += 1
+
+    div(left + 1, idx - 1)
+    div(idx, right)
+    print(root)
+
+
+def solution():
+    while True:
+        try:
+            data.append(int(input()))
+        except:
+            break
+    div(0, len(data) - 1)
+
+
+solution()
