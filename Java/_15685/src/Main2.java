bdc2f5209acfafe12bcb8e093c1e8453d2702fbc
Dltmd202 <dltmd202@gmail.com>
2023-06-17 03:23:27 +0900

    Java 15685

---
+++ Java/_15685/src/Main2.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        boolean[][] visited = new boolean[101][101];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+            int gen = Integer.parseInt(st.nextToken());
+
+            List<Integer> curves = new ArrayList<>();
+            curves.add(dir);
+
+            for (int j = 1; j <= gen; j++) {
+                int size = curves.size();
+                for (int k = size - 1; k >= 0; k--) {
+                    curves.add(getNextDirection(curves.get(k)));
+                }
+            }
+
+            int ny = y;
+            int nx = x;
+            visited[ny][nx] = true;
+
+            for (Integer ndir : curves) {
+                nx = nx + dx[ndir];
+                ny = ny + dy[ndir];
+
+                if(0 <= nx && nx <= 100 && 0 <= ny && ny <= 100)
+                    visited[ny][nx] = true;
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (visited[i][j] && visited[i][j + 1] && visited[i + 1][j] && visited[i + 1][j + 1]) {
+                    res++;
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    static int getNextDirection(int cur){
+        return (cur + 1) % 4;
+    }
+}
