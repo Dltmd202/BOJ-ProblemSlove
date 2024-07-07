35d3c87ceaf4204ae0c93061bda56108d900f3c3
dltmd202 <75921696+Dltmd202@users.noreply.github.com>
2024-07-07 21:58:37 +0900

    [level 3] Title: 주사위 고르기, Time: 5389.58 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,28 +1,51 @@
-from itertools import combinations, product
-from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict
+from itertools import combinations
 
-def solution(dices):
-    counter = {}
-    L = len(dices)
+def solution(dice):
+    answer = []
+    total_cnt = len(dice)
+    counter = Counter()
     
-    data = [3, 5, 8, 10, 15]
-    print(bisect_right(data, 6))
+    for d in dice:
+        d.sort()
     
-    
-    for a_indeies in combinations(range(L), L // 2):
-        b_indeies = tuple(set(range(L)) - set(a_indeies))
+    memo = defaultdict(Counter)
+
+
+    def search(counter, cur, idx):
+        if tuple(cur) in memo:
+            counter += memo[tuple(cur)]
+            return
+            
+        if idx == len(dice):
+            this_counter = Counter()
+            for c in combinations(list(enumerate(cur)), len(dice) // 2):
+                def get_my_score(c):
+                    score = 0
+                    for i, s in c:
+                        score += s
+                    return score
+
+                def get_this_id(c):
+                    ids = []
+                    for i, s in c:
+                        ids.append(i)
+                    return tuple(ids)
+                
+                total_sum = sum(cur)
+                my_score = get_my_score(c)
+                other_score = total_sum - my_score
+
+                if my_score > other_score:
+                    this_counter[get_this_id(c)] += 1
+            counter += this_counter
+            memo[tuple(cur)] = this_counter
+            return
         
-        a, b = [], []
-        for order in product(range(6), repeat=L//2):
-            # print(a_indeies)
-            a.append(sum(dices[i][j] for i, j in zip(a_indeies, order)))
-            b.append(sum(dices[i][j] for i, j in zip(b_indeies, order)))
-        b.sort()
-        
-        
-        wins = sum(bisect_left(b, num) for num in a)
-        counter[wins] = list(a_indeies)
+        for target in dice[idx]:
+            cur.append(target)
+            search(counter, cur, idx + 1)
+            cur.pop()
     
-    
-    max_key = max(counter.keys())
-    return [x + 1 for x in counter[max_key]]
+    search(counter, [], 0)
+    return list(map(lambda x: x + 1, counter.most_common()[0][0]))
